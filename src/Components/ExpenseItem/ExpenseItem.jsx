ce52fa6f039a39d3ad5cc08ed749a4af1adb0e34
Ankit Singh <ankit10226.as@gmail.com>
2024-04-13 20:02:57 +0530

    changes

--- src/Components/ExpenseItem/ExpenseItem.jsx
+++ src/Components/ExpenseItem/ExpenseItem.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({id,date,item,price}) => {  
+  const [itemName,setItemName] = useState(item);
+  const clickHandler = () =>{
+    let updatedItemName = prompt('Enter new Item Name : ');
+    setItemName(updatedItemName);
+  };
   return (
-    <div className="bg-gray-700 flex flex-wrap justify-between rounded-md my-3" id={id}>
+    <div className="bg-gray-700 flex flex-wrap justify-between rounded-md my-3 items-center" id={id}>
       <div className="my-3 mx-5 text-white">{date}</div>
-      <div className="my-3 mx-5 text-white">{item}</div>
+      <div className="my-3 mx-5 text-white">{itemName}</div>
       <div className="my-3 mx-5 text-white">{price}</div>
+      <button type="button" className="bg-slate-400 rounded-md my-3 mx-5 p-2" onClick={clickHandler}>click me</button>
     </div>
   );
 };
--- src/Pages/ExpenseItems/ExpenseItems.jsx
+++ src/Pages/ExpenseItems/ExpenseItems.jsx
@@ -3,16 +3,17 @@ import ExpenseItem from "../../Components/ExpenseItem/ExpenseItem";
 
 const ExpenseItems = () => {
   const expenseData = [
-    { date: new Date().toDateString(), itemDesc: "asdfdasfadfdf", price: 100 },
-    { date: new Date().toDateString(), itemDesc: "gjgdjsdjfgjf", price: 700 },
-    { date: new Date().toDateString(), itemDesc: "mbmvckrwjabjhg", price: 200 },
+    {id: 1, date: new Date().toDateString(), itemDesc: "asdfdasfadfdf", price: 100 },
+    {id: 2, date: new Date().toDateString(), itemDesc: "gjgdjsdjfgjf", price: 700 },
+    {id: 3, date: new Date().toDateString(), itemDesc: "mbmvckrwjabjhg", price: 200 },
   ];
   return (
     <div className="bg-gray-900 w-screen h-screen flex justify-center items-center">
       <div className="bg-gray-800 w-2/3 p-5 rounded-md">
-        {expenseData.map((expense, index) => (
+        {expenseData.map((expense) => (
           <ExpenseItem
-            id={index}
+            key={expense.id}
+            id={expense.id}
             date={expense.date}
             item={expense.itemDesc}
             price={expense.price}
