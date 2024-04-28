4a6daed7fb5e992054f4555ce7e8fa95bc0412a5
Ankit Singh <ankit10226.as@gmail.com>
2024-04-28 22:16:18 +0530

    changes

--- src/Components/AddToListComponents/AddNewItem/AddNewItem.jsx
+++ src/Components/AddToListComponents/AddNewItem/AddNewItem.jsx
@@ -1,35 +1,35 @@
 import React, { useState } from 'react'
+import styles from "./AddNewItem.module.css"; 
 
 const AddNewItem = ({getAddedItem}) => {
     const [newItem,setNewItem] = useState('');
-    const [errorBorder,setErrorBorder] = useState('');
+    const [errorBorder,setErrorBorder] = useState(true);
 
     const inputHandlerChange = (value) =>{
+        if(value != ''){ 
+            setErrorBorder(true);
+        }
         setNewItem(value); 
     }
 
     const submitFormHandler = (e) =>{
-        e.preventDefault(); 
+        e.preventDefault();         
         if(newItem == ''){
-            setErrorBorder('red');
+            setErrorBorder(false);
             return;
-        }else{
-            setErrorBorder('');
-        }
+        } 
 
-        const newAddedItem = newItem;
-
-        getAddedItem(newAddedItem);
+        getAddedItem(newItem);
         setNewItem('');
     };
   return (
-    <div>
         <form onSubmit={submitFormHandler}>
-            <h3 className='font-semibold text-slate-500 my-2'>Add New Item</h3>
-            <input type="text" name="newItem" id="newItem" className='shadow-lg border-2 rounded-md w-full px-2 py-1 font-semibold' value={newItem} onChange={(event)=>inputHandlerChange(event.target.value)} style={{borderColor:errorBorder}}/>
-            <button type="submit" className="rounded-md py-1 px-4 border bg-slate-400 text-white my-2">Add Item</button>
+            <div className={`${!errorBorder ? styles.invalid : ''}`}>
+                <h3 className='font-semibold text-slate-500 my-2'>Add New Item</h3>
+                <input type="text" name="newItem" id="newItem" className='shadow-lg border-2 rounded-md w-full px-2 py-1 font-semibold' value={newItem} onChange={(event)=>inputHandlerChange(event.target.value)}/>
+                <button type="submit" className="rounded-md py-1 px-4 border bg-slate-400 text-white my-2">Add Item</button>
+            </div>
         </form>
-    </div>
   )
 }
 
